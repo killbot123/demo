2fc040a18c3bc00c260fc5461693f6b27047d0c8
killbot123 <46462974+killbot123@users.noreply.github.com>
2022-09-06 09:08:22 -0500

    Update startup.sh

--- CCM/startup.sh
+++ CCM/startup.sh
@@ -3,3 +3,5 @@ echo "I am the CCM"
 whoami
 echo "PATH is $PATH"
 echo "HOME is $HOME"
+nmcli c add	type ethernet	conn.id eth1 ifname eth1 ipv4.method manual ipv4.address "10.10.10.4/24"
+echo "10.10.10.5  umc" >> /etc/hosts
